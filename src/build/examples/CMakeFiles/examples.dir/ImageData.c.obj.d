7089157cf89ff0fa58bcadf572b875d1d3db3efd
Twip4 <paulbaudinot@gmail.com>
2023-11-09 16:52:55 +0100

    SRC lib and main

---
+++ src/build/examples/CMakeFiles/examples.dir/ImageData.c.obj.d
@@ -0,0 +1,3 @@
+examples/CMakeFiles/examples.dir/ImageData.c.obj: \
+ /Users/paulbaudinot/Documents/Killer-Whale/src/examples/ImageData.c \
+ /Users/paulbaudinot/Documents/Killer-Whale/src/examples/ImageData.h
---
+++ src/build/lib/Fonts/CMakeFiles/Fonts.dir/font20.c.obj.d
@@ -0,0 +1,10 @@
+lib/Fonts/CMakeFiles/Fonts.dir/font20.c.obj: \
+ /Users/paulbaudinot/Documents/Killer-Whale/src/lib/Fonts/font20.c \
+ /Users/paulbaudinot/Documents/Killer-Whale/src/lib/Fonts/fonts.h \
+ /Applications/ArmGNUToolchain/13.2.Rel1/arm-none-eabi/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ /Applications/ArmGNUToolchain/13.2.Rel1/arm-none-eabi/arm-none-eabi/include/stdint.h \
+ /Applications/ArmGNUToolchain/13.2.Rel1/arm-none-eabi/arm-none-eabi/include/machine/_default_types.h \
+ /Applications/ArmGNUToolchain/13.2.Rel1/arm-none-eabi/arm-none-eabi/include/sys/features.h \
+ /Applications/ArmGNUToolchain/13.2.Rel1/arm-none-eabi/arm-none-eabi/include/_newlib_version.h \
+ /Applications/ArmGNUToolchain/13.2.Rel1/arm-none-eabi/arm-none-eabi/include/sys/_intsup.h \
+ /Applications/ArmGNUToolchain/13.2.Rel1/arm-none-eabi/arm-none-eabi/include/sys/_stdint.h
